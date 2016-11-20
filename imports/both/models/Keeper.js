f1d694a1a8d7f61e4933e871111ff8495d13c16f
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2016-11-21 04:16:57 +0800

    feat(Keeper.js): The keeper can distinguish whether a team is an enemy or not.

--- imports/both/models/Keeper.js
+++ imports/both/models/Keeper.js
@@ -24,6 +24,12 @@ class Keeper extends Player {
     return Goal.find({ type: 'blocked', keeperId: this._id }).count()
   }
 
+  getEnemyChasers(team1, team2) {
+    const teams = [team1, team2]
+    const enemyTeam = teams.find(team => (team.keeperId !== this._id))
+    return enemyTeam.chasers
+  }
+
 }
 
 export default Keeper
