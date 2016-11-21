2a2b28ce2f3206acdd2df065221525d71c762d0b
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2016-11-22 04:50:43 +0800

    fix(Seeker): Changed snitch attribute to seekerId
    Maybe I forgot to change this one, I've replaced all attributes to make them uniform and not
    
    confusing.

--- imports/both/models/Seeker.js
+++ imports/both/models/Seeker.js
@@ -10,12 +10,11 @@ import SetupCollection from '../decorators/SetupCollection'
 class Seeker extends Player {
 
   catch(snitch) {
-    snitch.playerId = this._id
+    snitch.seekerId = this._id
     snitch.caught = new Date
     snitch.save(() => {
       const eventId = Event.insert({
         notificationType: 'snitch caught',
-        seekerId: this._id,
         snitchId: snitch._id,
         date: snitch.caught,
       }, () => {
@@ -26,7 +25,6 @@ class Seeker extends Player {
   }
 
   get score() {
-    // plus 30 points if the snitch is caught
     return Snitch.find({ seekerId: this._id }).count() * 30
   }
 
