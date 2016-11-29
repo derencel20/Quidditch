7a23e95fd0cbeba9ddd050b414063dc17665d9fd
Vince Paul Dela Cruz <sekiryuutei47@gmail.com>
2016-11-30 02:26:01 +0800

    chore(server and client js): Removed lines that prevent eslint error
    I removed them since they're already fixed, the error that is.

--- both/main.js
+++ both/main.js
@@ -1,9 +1,6 @@
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
 /* eslint-disable import/no-absolute-path */
 
 import '/imports/both/models/Game.js'
 import '/imports/both/models/Team.js'
 import '/imports/both/models/Player.js'
 import '/imports/both/models/Snitch.js'
-import '/imports/both/models/Event.js'
--- client/main.js
+++ client/main.js
@@ -1,6 +1,4 @@
 /* eslint-disable import/no-absolute-path */
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
 
 import '/imports/client/controllers/MainComponent'
 import '/imports/client/controllers/GamesListComponent'
